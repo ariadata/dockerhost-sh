3e89ee9b6198a3ea04ffec1a7a355a989acdae5e
Mehrdad Amini <mehrdad@arzinja.dev>
2022-08-23 03:30:50 +0300

    Create dockerhost-basic-debian-11-root.sh

---
+++ dockerhost-basic-debian-11-root.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+clear
+
+function get_latest_github_release_number() {
+	curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+read -e -p $'Update && Upgrade System first [y/n]? : ' -i "y" if_update_first
+
+if [[ $if_update_first =~ ^([Yy])$ ]]
+then
+	apt --yes update && apt -q --yes upgrade
+fi
+
+apt --yes install wget curl git nano lsb-release sqlite3 p7zip gnupg-agent apt-transport-https ca-certificates software-properties-common cron
+systemctl enable --now cron
+
+
+bash <(curl -sSL -fsSL https://get.docker.com)
+chmod 666 /var/run/docker.sock
+systemctl enable --now docker
+
+docker_compose_latest_version="$(get_latest_github_release_number docker/compose)"
+curl -L "https://github.com/docker/compose/releases/download/$docker_compose_latest_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+
+apt --yes update && apt -q --yes upgrade && apt --yes autoremove
+
+clear
+
+echo -e $'\n=====================================\n\e[32mDockerHost + docker-compose successfully installed.\033[0m\n=====================================\n'
+## reboot at the end
+read -e -p $'Do you want to \e[31mreboot now\033[0m ? : ' -i "y" if_reboot_at_end
+if [[ $if_reboot_at_end =~ ^([Yy])$ ]]
+then
+	reboot
+	exit
+fi
