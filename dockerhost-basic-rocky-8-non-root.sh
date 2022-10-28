ab7679aadcbefd716a2a545fce2bf531e7c2112a
Mehrdad Amini <pcmehrdad@gmail.com>
2022-10-28 04:31:41 +0300

    add basic for rocky-8

---
+++ dockerhost-basic-rocky-8-non-root.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -e
+clear
+
+function get_latest_github_release_number() {
+	curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+# Change System timezone
+read -e -p $'Set Default System Timezone : ' -i "Europe/Istanbul" system_default_timezone
+sudo timedatectl set-timezone $system_default_timezone
+
+sudo sed -i 's/SELINUX=[^\s]+/SELINUX=disabled/g' /etc/selinux/config
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo yum makecache --refresh
+
+sudo yum update -y
+
+sudo yum install -y wget curl git nano sqlite p7zip ca-certificates yum-utils
+
+sudo yum config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo chmod 666 /run/docker.sock
+sudo systemctl enable --now docker
+
+docker_compose_latest_version="$(get_latest_github_release_number docker/compose)"
+curl -L "https://github.com/docker/compose/releases/download/$docker_compose_latest_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+
+sudo yum update -y && sudo yum autoremove -y
+
+clear
+
+echo -e $'\n=====================================\n\e[32mDockerHost successfully installed.\033[0m\n=====================================\n'
+## reboot at the end
+read -e -p $'Do you want to \e[31mreboot now\033[0m ? : ' -i "y" if_reboot_at_end
+if [[ $if_reboot_at_end =~ ^([Yy])$ ]]
+then
+	reboot
+	exit
+fi
